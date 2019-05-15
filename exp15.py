7127da8ffb68f3f98a0b29deff3d48c525a3a273
sangeethagopi <50619394+sangeethagopi@users.noreply.github.com>
2019-05-15 16:43:23 +0530

    Update exp15.py

--- exp15.py
+++ exp15.py
@@ -1,6 +1,6 @@
-lower = int(input())
-upper = int(input())
-for num in range(lower,upper + 1):
+a = int(input())
+b = int(input())
+for num in range(a,b + 1):
    if num > 1:
        for i in range(2,num):
            if (num % i) == 0:
